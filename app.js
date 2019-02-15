094e37d3be1735cdee7a3be57ef201d0f16d6aea
Jordis Man <jordisman@gmail.com>
2019-02-14 21:41:47 -0800

    update array

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 //if use item = '', last item will get over written
-var items = ['Buy Food', 'Walk the dog', 'Do laundry'];
+var items = [];
 
 app.get('/', function(req, res) {
   var today = new Date();
