e65b2219d609cc24b8ba75f522be680fc421f8c8
Jordis Man <jordisman@gmail.com>
2019-02-14 21:37:16 -0800

    update

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-// const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
-var items = ['Buy Food', 'Cook Food', 'Eat Food'];
+//if use item = '', last item will get over written
+var items = ['Buy Food', 'Walk the dog', 'Do laundry'];
 
 app.get('/', function(req, res) {
   var today = new Date();
+  // var currentDay = today.getDay();
+  //The getDay() method returns the day of the week
+  // var day = '';
+
+// if (currentDay === 0) {
+//   day = 'Sunday';
+// } else if (currentDay === 6) {
+//   day = 'Saturday';
+// } else if (currentDay === 5) {
+//   day = 'Friday';
+// } else if (currentDay === 4) {
+//   day = 'Thursday';
+// } else if (currentDay === 3) {
+//   day = 'Wednesday';
+// } else if (currentDay === 2) {
+//   day = 'Tuesday';
+// } else if (currentDay === 1) {
+//   day = 'Monday';
+// }
+  //https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+
   var options = {
     weekday: 'long',
     day: 'numeric',
@@ -18,7 +41,8 @@ app.get('/', function(req, res) {
   };
 
   var day = today.toLocaleDateString('en-US', options);
-
+  //res.render(view [, locals] [, callback])
+  //locals: an obj whose props define local variables for the view
   res.render('list', {
     kindOfDay: day,
     newListItems: items
@@ -26,10 +50,12 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  var item = req.body.newItem; //grab hold of newItem
+  //console.log('req.body = ', req.body)
+  var item = req.body.newItem; //input name="newItem"
 
   items.push(item);
-  res.redirect('/');
+  //cant use res.render('list', {kindOfDay: day})
+  res.redirect('/'); //when post is triggered, go back to get /
 });
 
 app.listen(3000, function() {
